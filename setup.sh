fdb1d2c86982528b5a3fdb41bd0fb31b054221fe
bertrandvidal <bert.vidal@oracle.com>
2020-07-26 12:11:22 -0400

    Add repeatable setup file
    Will create a virtualenv with python3, install pygame deps and compile
    pygame from source because macOS
    
    Tests:
    - was able to rebuild from scratch and run tetris.py

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create virtualenv to not mess up your machine
+virtualenv -q -p python3 env-tetris-ai
+source env-tetris-ai/bin/activate
+
+# install pygame dependencies
+brew install sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_net sdl2_ttf
+
+# download, configure and install pygame
+git clone -b 1.9.6 https://github.com/pygame/pygame.git .pygame
+python .pygame/setup.py -config -auto -sdl2
+python .pygame/setup.py install
