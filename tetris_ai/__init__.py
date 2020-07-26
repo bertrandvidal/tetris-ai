aaaea15378643034dee1080ccae8b9c2ed790404
bertrandvidal <bert.vidal@oracle.com>
2020-07-26 14:55:19 -0400

    Apply formatting

--- tetris_ai/__init__.py
+++ tetris_ai/__init__.py
@@ -1,6 +1,5 @@
 from gym.envs.registration import register
 
 register(
-    id='tetris_gym-v0',
-    entry_point='tetris_ai.envs:TetrisEnv',
+    id="tetris_gym-v0", entry_point="tetris_ai.envs:TetrisEnv",
 )
