422447c0948ea23547477cbbeb8c25adb06c78df
bertrandvidal <bert.vidal@oracle.com>
2020-07-26 14:54:09 -0400

    Add black as a dependency
    Because I don't want to do any compromise on my code format

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 virtualenv -q -p python3 env-tetris-ai
 source env-tetris-ai/bin/activate
 python -m pip install --upgrade pip
+pip install black
 
 # install pygame dependencies
 brew install sdl2 sdl2_gfx sdl2_image sdl2_mixer sdl2_net sdl2_ttf
