bc1fd277c1c635bb16f67be257286892f3692b5b
bertrandvidal <bert.vidal@oracle.com>
2020-07-29 18:47:13 -0400

    Test before you commit Bert

--- tetris_ai/play.py
+++ tetris_ai/play.py
@@ -6,7 +6,7 @@ from tetris_ai.train import get_agent
 
 if __name__ == "__main__":
     env = gym.make("tetris_ai:tetris_gym-v0")
-    agent = get_agent()
+    agent = get_agent(env)
     agent.load_weights(os.path.abspath(sys.argv[1]))
     # Finally, evaluate our algorithm for 5 episodes.
     agent.test(env, nb_episodes=5, visualize=True, verbose=0)
